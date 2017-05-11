459fdda6ff4aa686a18c23e78688968ca1b0ebb4
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-05-11 17:15:12 +0100

    doc

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # tic-tac-toe
 
-A Clojure library designed to ... well, that part is up to you.
 
 ## Usage
 
-FIXME
+
 
 ## License
 
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tic-tac-toe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Sample tic-tac-toe implementation"
+  :url "https://github.com/AndreaCrotti/tic-tac-toe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
