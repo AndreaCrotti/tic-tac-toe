7352e702f2c0a72fd0c846af1ba7891f144d2230
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-05-11 17:01:31 +0100

    first commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject tic-tac-toe "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [environ "1.1.0"]
+                 [net.mikera/core.matrix "0.59.0"]
+                 [org.clojure/core.match "0.2.2"]
+                 [org.clojure/core.unify "0.5.7"]
+                 [org.hugoduncan/core.logic "0.8.11.1"]])
