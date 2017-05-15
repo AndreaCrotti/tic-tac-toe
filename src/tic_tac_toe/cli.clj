169d621acefee05fd77fa5ec00139b9199a4a7f9
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-05-15 09:51:47 +0100

    add a multimethod to dispatch to the right thing

--- src/tic_tac_toe/cli.clj
+++ src/tic_tac_toe/cli.clj
@@ -2,14 +2,37 @@
   (:require [clojure.tools.cli :refer [parse-opts]]
             [tic-tac-toe.core :as core]))
 
+(defmulti tic-tac-run
+  "Dispatch on algorithm and modality"
+  :algorithm)
+
+;; TODO: this should be moved in core and tested accordingly
+(defmethod tic-tac-run :random
+  [this]
+  (core/fill-board-randomly))
+
+(defmethod tic-tac-run :semi
+  [this]
+  (println "Run semi mode"))
+
 (def cli-options
-  [["-s" "--size" "Board size to use"
+  [["-m" "--mode" "Different modality to use"
+    :choices ["auto" "human"]
+    :default "auto"]
+
+   ["-a" "--algorithm" "Algorithm to use"
+    :choices ["random" "semi" "full"]
+    :default "random"]
+
+   ["-s" "--size" "Board size to use"
     :default core/DEFAULT-BOARD-SIZE
     :validate [#(pos? (read-string %))]]])
 
 (defn -main
   [& args]
   (let [parsed-args (parse-opts args cli-options)
-        board-size (-> parsed-args :options :size)]
+        board-size (-> parsed-args :options :size)
+        algorithm (-> parsed-args :options :algorithm keyword)]
+    
     (println "Welcome to the tic-tac-toe implementation, board size " board-size)
-    (core/fill-board-randomly)))
+    (tic-tac-run {:algorithm algorithm})))
