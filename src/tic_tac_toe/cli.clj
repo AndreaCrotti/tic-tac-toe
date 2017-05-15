523e33c048675bcd8659bda3b5c0910401ce7943
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-05-15 11:50:48 +0100

    add edn config and parse it

--- src/tic_tac_toe/cli.clj
+++ src/tic_tac_toe/cli.clj
@@ -1,38 +1,24 @@
 (ns tic-tac-toe.cli
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [tic-tac-toe.core :as core]))
+            [tic-tac-toe.core :as core]
+            [clojure.edn :as edn]))
 
-(defmulti tic-tac-run
-  "Dispatch on algorithm and modality"
-  :algorithm)
-
-;; TODO: this should be moved in core and tested accordingly
-(defmethod tic-tac-run :random
-  [this]
-  (core/fill-board-randomly))
-
-(defmethod tic-tac-run :semi
-  [this]
-  (println "Run semi mode"))
+(def ^:const DEFAULT-CONFIG-FILE "resources/config.edn")
 
 (def cli-options
-  [["-m" "--mode" "Different modality to use"
-    :choices ["auto" "human"]
-    :default "auto"]
+  [["-c" "--config" "Config file to load"
+    :default DEFAULT-CONFIG-FILE]
 
-   ["-a" "--algorithm" "Algorithm to use"
-    :choices ["random" "semi" "full"]
-    :default "random"]
-
-   ["-s" "--size" "Board size to use"
-    :default core/DEFAULT-BOARD-SIZE
-    :validate [#(pos? (read-string %))]]])
+   ["-p" "--profile" "Profile to run given the config file"
+    :default "computer-computer-random"]])
 
 (defn -main
   [& args]
   (let [parsed-args (parse-opts args cli-options)
-        board-size (-> parsed-args :options :size)
-        algorithm (-> parsed-args :options :algorithm keyword)]
+        config-file (-> parsed-args :options :config)
+        profile (-> parsed-args :options :profile)
+        config (-> config-file slurp edn/read-string)
+        game-config (get config (keyword profile))]
     
-    (println "Welcome to the tic-tac-toe implementation, board size " board-size)
-    (tic-tac-run {:algorithm algorithm})))
+    (core/fill-board-randomly)))
+
